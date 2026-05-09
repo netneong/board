97b89e0fb908ee7cd088c3f4894bda38845763d2
jiyong <pjy200407@gmail.com>
2026-05-09 23:39:18 +0900

    CRUD 완성(게시판 완성)

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-posts = [{'id': 1, 'title': '첫 번째 글', 'author': '홍길동', 'date': '2026-03-22 11:34'},
-        {'id': 2, 'title': '두 번째 글', 'author': '김철수', 'date': '2026-03-22 22:12'},]
+posts = [
+    {'id': 1, 'title': '첫 번째 글', 'author': '홍길동', 'content': '첫 번째 글의 내용입니다.', 'date': '2026-03-22 11:34'},
+    {'id': 2, 'title': '두 번째 글', 'author': '김철수', 'content': '두 번째 글의 내용입니다.', 'date': '2026-03-22 22:12'},
+]
 next_id = 3
 
 # 바인딩: 식별자와 객체를 연결하는 것을 말한다.
@@ -52,6 +54,35 @@ def detail(post_id):
     
     # 찾은 글을 detail.html로 넘기기
     return render_template('detail.html', post=post)
+
+@app.route('/edit/<int:post_id>', methods =['GET', 'POST'])
+def edit(post_id):
+    post = next((p for p in posts if p['id'] == post_id), None)
+    
+    if post is None:
+        return "글을 찾을 수 없습니다.", 404
+    
+    if request.method == 'POST': # 데이터를 실제로 저장
+        title = request.form.get('title')
+        author = request.form.get('author')
+        content = request.form.get('content')
+        post['title'] = title
+        post['author'] = author
+        post['content'] = content
+       
+        return redirect(url_for('detail', post_id=post_id)) # url_for()는 함수 이름을 문자열로 받아 바인딩되어있는 URL을 반환한다., redirect()는 브라우저에 이동명령을 전달한다.
+    else:
+        return render_template('edit.html', post=post) # 수정 화면을 보여주는 get요청
+
+@app.route('/delete/<int:post_id>', methods=['POST'])
+def delete(post_id):
+    global posts
+    
+    # 리스트 컴프리헨션: post_id가 일치하지 않는 항목들만 남겨서 posts를 갱신합니다.
+    posts = [post for post in posts if post['id'] != post_id]
+    
+    # 삭제 후 메인 페이지(또는 목록 페이지)로 돌아갑니다.
+    return redirect(url_for('dashboard'))        
 
 if __name__ == '__main__':
     app.run(debug=True) # 코드 저장할 때마다 서버 자동 재시작
